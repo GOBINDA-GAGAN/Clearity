702997066b8f8f4354f156f7b527c45b8f8711f2
Gobinda-Gagan <gobindagagandey@gmail.com>
2026-01-06 18:06:49 +0530

    refactor: clean up like routes by separating unlikeBlog import

--- Backend/src/controllers/v1/like/like_blog.ts
+++ Backend/src/controllers/v1/like/like_blog.ts
@@ -1,6 +1,4 @@
 import { Request, Response } from "express";
-import { User } from "@/models/user"
-import config from "@/config/config";
 import { Like } from "@/models/likes";
 import { Blog } from "@/models/blog";
 
@@ -56,37 +54,5 @@ export const likeBlog = async (req: Request, res: Response): Promise<void> => {
 };
 
 
-export const unlikeBlog = async (req: Request, res: Response): Promise<void> => {
-  try {
 
-    const limit = parseInt(req.query.limit as string) ?? config.defaultResLimit
-    const offset = parseInt(req.query.offset as string) ?? config.defaultResOffset;
-    const users = await User.find().select("-password -__v ").limit(limit).skip(offset).lean().exec();
-    const total = await User.countDocuments();
-
-
-    if (users.length === 0) {
-      res.status(404).json({
-        code: "not found",
-        message: " No User ddd found"
-      })
-      return;
-    }
-
-    res.status(202).json({
-      total,
-      offset, limit, users
-    })
-
-  } catch (error) {
-    console.error("❌ Error in  like blog Controller:", error);
-
-    res.status(500).json({
-      success: false,
-      controller: "registerUser",
-      message: "Internal Server Error",
-    });
-    return;
-  }
-};
 
